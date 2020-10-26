485d5fd7164227310a6541864637d31f9e17ad02
mrautenberg <mrautenberg@outlook.com>
2020-10-26 15:40:09 +0100

    functionality updates form submit

--- app.js
+++ app.js
@@ -1,147 +1,144 @@
-const input = document.querySelectorAll('input');
-const options = document.querySelectorAll('.option-container');
+const input = document.querySelectorAll("input");
+const options = document.querySelectorAll(".option-container");
+const submitBtn = document.querySelector(".submit-btn");
 
+let score = [];
 
-let score = 0;
-
-options.forEach(function (cur) {
+/* options.forEach(function (cur) {
     cur.addEventListener('click', inputCheck);
 });
-
-
+ */
 
 // options.addEventListener('click', inputCheck);
 
-function inputCheck(e) {
+/* function inputCheck(e) {
     e.preventDefault();
 
     input.checked = true;
     console.log(input);
-}
-
+} */
 
 let questions = [
-    {
-        question: 'Vilken veckodag är det idag?',
-        answers: {
-            a: 'Måndag',
-            b: 'Tisdag',
-            c: 'Onsdag',
-            d: 'Lördag'
+  {
+    question: "Vilken veckodag är det idag?",
+    answers: {
+      a: "Måndag",
+      b: "Tisdag",
+      c: "Onsdag",
+      d: "Lördag",
     },
-        correctAns: 'a'
+    correctAns: "a",
+  },
+  {
+    question: "Vilken juice är godast?",
+    answers: {
+      a: "Apelsin",
+      b: "Äpple",
+      c: "Ananas",
+      d: "Gurka",
     },
-    {
-        question: 'Vilken juice är godast?',
-        answers: {
-            a: 'Apelsin',
-            b: 'Äpple',
-            c: 'Ananas',
-            d: 'Gurka'
+    correctAns: "c",
+  },
+  {
+    question: "Vilken dagbok tillhör Bert?",
+    answers: {
+      a: "Berts",
+      b: "Åkes",
+      c: "Lill-Eriks",
+      d: "Maggans",
     },
-        correctAns: 'c'
+    correctAns: "a",
+  },
+  {
+    question: "Vilken fot sitter till höger?",
+    answers: {
+      a: "Höger foten",
+      b: "Vänster foten",
+      c: "Båda",
+      d: "Ingen",
     },
-    {
-        question: 'Vilken dagbok tillhör Bert?',
-        answers: {
-            a: 'Berts',
-            b: 'Åkes',
-            c: 'Lill-Eriks',
-            d: 'Maggans'
+    correctAns: "d",
+  },
+  {
+    question: "Vilken båt är längst?",
+    answers: {
+      a: "Yatchen",
+      b: "Ekan",
+      c: "Färjan",
+      d: "Jollen",
     },
-        correctAns: 'a'
-    },
-    {
-        question: 'Vilken fot sitter till höger?',
-        answers: {
-            a: 'Höger foten',
-            b: 'Vänster foten',
-            c: 'Båda',
-            d: 'Ingen'
-    },
-        correctAns: 'd'
-    },
-    {
-        question: 'Vilken båt är längst?',
-        answers: {
-            a: 'Yatchen',
-            b: 'Ekan',
-            c: 'Färjan',
-            d: 'Jollen'
-    },
-        correctAns: 'b'
-    }
+    correctAns: "b",
+  },
 ];
 
-
-// @TODO: lägga till värdet på ID för ny fråga som genereras.
-// just nu kommer man till fr 1 om man klickar på label på fråga 2,3,4 osv
-
-// @TODO (onödigt): det skulle vara kul att generera slumpmässigt tal för hexkod å slumpa färg för fråga 
-
 function createQuestionsHTML() {
-    let sections = [];
+  let sections = [];
 
-    for (let i = 0; i < questions.length; i++) {
+  for (let i = 0; i < questions.length; i++) {
+    let section = document.createElement("section");
+    section.id = `question${i + 1}`;
 
-        let section = document.createElement('section');
-        section.id =`question${i+1}`;
-
-        let HTML =
-            `<div class="question-card">
+    let HTML = `<div class="question-card">
                 <h3 id="question" class="question">${questions[i].question}</h3>
-                <h4 class="question-number">Fråga ${i+1}</h4>
+                <h4 class="question-number">Fråga ${i + 1}</h4>
                 <div class="option-container">
-                    <input type="radio" name="opt1" id="opt1">
-                    <label for="opt1" class="option">${questions[i].answers.a}</label>
+                    <input type="radio" name="opt${i + 1}" id="opt1">
+                    <label for="opt1" class="option">${
+                      questions[i].answers.a
+                    }</label>
                 </div>
                 <div class="option-container">
-                    <input type="radio" name="opt1" id="opt2">
-                    <label for="opt2" class="option">${questions[i].answers.b}</label>
+                    <input type="radio" name="opt${i + 1}" id="opt2">
+                    <label for="opt2" class="option">${
+                      questions[i].answers.b
+                    }</label>
                 </div>
                 <div class="option-container">
-                    <input type="radio" name="opt1" id="opt3">
-                    <label for="opt3" class="option">${questions[i].answers.c}</label>
+                    <input type="radio" name="opt${i + 1}" id="opt3">
+                    <label for="opt3" class="option">${
+                      questions[i].answers.c
+                    }</label>
                 </div>
                 <div class="option-container">
-                    <input type="radio" name="opt1" id="opt4">
-                    <label for="opt4" class="option">${questions[i].answers.d}</label>
+                    <input type="radio" name="opt${i + 1}" id="opt4">
+                    <label for="opt4" class="option">${
+                      questions[i].answers.d
+                    }</label>
                 </div>`;
 
-        // går det att baka ihop html prev och next?
-        let HTMLBtns = `<div class="buttons"><button class="btn" onclick="window.location.href='#question${i+2}'">Nästa fråga</button> <button class="btn" onclick="window.location.href='#question${i}'">
-        Tillbaka</button> </div>`;   
-        
-        // let HTMLPrev = `<button class="btn" onclick="window.location.href='#question${i}'">
-        // Tillbaka</button>`;
+    let HTMLNext = `<button class="btn" onclick="window.location.href='#question${
+      i + 2
+    }'">Nästa fråga</button>`;
 
-        let HTMLendSec = `</div></div>`;
+    let HTMLPrev = `<button class="btn" onclick="window.location.href='#question${i}'">
+        Tillbaka</button>`;
 
-        if(i === 0) {
-            section.innerHTML = HTML + HTMLBtns+ HTMLendSec;
-        } else if (i === questions.length - 1) {
-            section.innerHTML = HTML + HTMLBtns + HTMLendSec;
-        } else {
-            section.innerHTML = HTML + HTMLBtns + HTMLendSec;
-        }
+    let HTMLsubmit = `<button class="btn submit-btn">Rätta</button>`;
 
-        section.style.backgroundColor = setBg();
-        console.log( section.style.backgroundColor);
+    let HTMLendSec = `</div></div>`;
 
-        sections.push(section);
-
+    if (i === 0) {
+      section.innerHTML = HTML + HTMLNext + HTMLendSec;
+    } else if (i === questions.length - 1) {
+      section.innerHTML = HTML + HTMLPrev + HTMLsubmit + HTMLendSec;
+    } else {
+      section.innerHTML = HTML + HTMLPrev + HTMLNext + HTMLendSec;
     }
-    
-    return sections;
+
+    section.style.backgroundColor = setBg();
+
+    sections.push(section);
+  }
+
+  return sections;
 }
 
 function displayQuestion() {
+  let sections = createQuestionsHTML();
 
-    let sections = createQuestionsHTML();
-    console.log(sections[0]);
-    
-    sections.forEach(section => document.querySelector('.questions').appendChild(section));
-
+  sections.forEach((section) =>
+    document.querySelector(".questions").appendChild(section)
+  );
 }
 
 displayQuestion();
@@ -153,18 +150,50 @@ displayQuestion();
 // Ifall fel svar, inget händer!
 // Output av score function presenteras
 
-
-
 function setBg() {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = `#${randomColor}`;
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = `#${randomColor}`;
 
-    return `#${randomColor}`;
-    // color.innerHTML = `#${randomColor}`;
+  return `#${randomColor}`;
+}
+
+// onload?
+window.addEventListener("load", setBg);
+
+setBg();
+
+//   submitBtn.addEventListener('click', (e) => {
+//       console.log(e.target);
+//   });
+
+const lastQuestion = document.getElementById(`question${questions.length}`);
+
+lastQuestion.addEventListener("click", (e) => {
+  const { target } = e;
+
+  // ta bort alla event förutom submit
+  if (!target.classList.contains("submit-btn")) {
+    return;
   }
-  
-  // onload? 
-  window.addEventListener("load", setBg);
-  
-  setBg();
-  
+
+  correct();
+  // rätta svar funktion
+});
+
+function correct() {
+  let corrAns = questions.map((el) => el.correctAns);
+  console.log(corrAns);
+
+  const quest = document.querySelector(".questions");
+
+  const checked = quest.querySelectorAll("input[type=radio]:checked");
+
+  const checkAnswers = checked.forEach((element) => {
+    console.log(element);
+  });
+
+  conbsole.log(checkAnswers);
+
+  console.log(checked);
+  // nodelist med index går att jmf med svar? correct answers array
+}
